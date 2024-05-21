da74a6a0d16161b4128f173e95bc33a3426c0a03
Chris <marte.christofer@gmail.com>
2024-05-21 14:57:03 -0400

    Added base database functionality

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import psycopg2
 
 def main():
+    
+    conn = psycopg2.connect("dbname=test user=postgres")
+    cur = conn.cursor()
+    cur.execute("SELECT * from my_data")
+    records = cur.fetchall()
+    
+    
     print("Start of the Journey!")
     
 main()
