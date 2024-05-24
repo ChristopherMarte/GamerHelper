e4dbec58ad34304040e84e958832c96a95ef90ec
Chris <marte.christofer@gmail.com>
2024-05-24 11:14:55 -0400

    Minor changes (username and tables)

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import psycopg2
 
 def main():
     
-    conn = psycopg2.connect("dbname=test user=postgres")
+    conn = psycopg2.connect("dbname=test user=test")
     cur = conn.cursor()
-    cur.execute("SELECT * from my_data")
+    cur.execute("SELECT * from data")
     records = cur.fetchall()
-    
     
     print("Start of the Journey!")
     
