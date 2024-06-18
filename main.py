03f050f56430028a9265a3442a3630c5cf8ac619
Chris <marte.christofer@gmail.com>
2024-06-18 18:38:06 -0400

    DB Work

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import psycopg2
 
 def main():
     
-    conn = psycopg2.connect("dbname=gamerhelper user=test")
+    conn = psycopg2.connect("dbname=gamerhelper user=test password=password")
     cur = conn.cursor()
-    cur.execute("SELECT * from games")
+    cur.execute("SELECT * from users")
     records = cur.fetchall()
     
     print(records)
